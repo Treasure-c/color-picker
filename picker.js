6221b28ae455c9ef2b161da552a7bb65a9e7750d
chikwendu Treasure <chikwendutreasure2019@gmail.com>
2024-10-15 14:33:12 +0100

    color-picker

---
+++ picker.js
@@ -0,0 +1,40 @@
+const first = document.getElementById("lightsalmon")
+first.addEventListener("click", lightsalmon)
+
+function lightsalmon() {
+    document.getElementById("body").style.backgroundColor="lightsalmon"
+}
+// second
+const second = document.getElementById("olivedrab")
+    second.addEventListener("click", olivedrab)
+function olivedrab() {
+    document.getElementById("body").style.backgroundColor="olivedrab"
+}
+// third
+function goldenrod() {
+    document.getElementById("body").style.backgroundColor="goldenrod"
+} 
+// fourth
+function royalblue() {
+    document.getElementById("body").style.backgroundColor="royalblue"
+}
+// fifth
+function paleturquoise() {
+    document.getElementById("body").style.backgroundColor="paleturquoise"
+}
+// sixth
+function pink() {
+    document.getElementById("body").style.backgroundColor="pink"
+}
+// seventh
+function cyan() {
+    document.getElementById("body").style.backgroundColor="cyan"
+}
+// eighth
+function gray() {
+    document.getElementById("body").style.backgroundColor="gray"
+}
+// reset
+function reset() {
+    document.getElementById("body").style.background="none"
+}
